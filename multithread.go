ac8b276907284824c55c10d562581f8ad4c5bf11
bootjp <oh@bootjp.me>
2019-04-07 13:46:11 +0900

    wip

--- multithread.go
+++ multithread.go
@@ -1,55 +1,109 @@
 package main
 
 import (
+	"fmt"
 	"math"
+	"math/big"
+	"math/rand"
 	"os"
 	"runtime"
-	"strconv"
+	"sync"
+	"time"
 )
 
 const (
-	_  = iota
-	KB = 1 << (10 * iota)
-	MB = 1 << (10 * iota)
+	limit     = uint64(math.MaxUint32)
+	calcRange = 1000
 )
 
-const (
-	chanSize = KB
-	bufSize  = MB
-)
+type SafeNum struct {
+	n uint64
+	m sync.RWMutex
+}
+
+func (s *SafeNum) Get() uint64 {
+	s.m.Lock()
+	tmp := s.n
+
+	s.n += calcRange
+
+	s.m.Unlock()
+	return tmp
+}
 
 func main() {
-	thread := uint64(runtime.NumCPU())
-	c := make(chan uint64, chanSize)
-	for i := uint64(3); i < uint64(2*thread+3); i += 2 {
-		go prime(i, thread, c)
+	s := &SafeNum{ /*n: 2*/ }
+	thread := uint64(runtime.NumCPU() * 2)
+	//thread := uint64(1)
+
+	wg := &sync.WaitGroup{}
+
+	for i := uint64(0); i < thread; i++ {
+		wg.Add(1)
+		go prime(s, wg)
 	}
-	buf := make([]byte, 0)
-	for {
-		buf = append(buf, strconv.FormatUint(<-c, 10)...)
-		buf = append(buf, '\n')
-		if len(buf) > bufSize {
-			os.Stdout.Write(buf)
-			buf = make([]byte, 0)
-		}
-	}
+	wg.Wait()
 }
 
-func prime(n uint64, thread uint64, c chan uint64) {
-	increment := uint64(2 * thread)
-	max := uint64(math.MaxUint64 - increment)
-	for i := n; i < max; i += increment {
-		if isPrime(i) {
-			c <- i
+func prime(s *SafeNum, wg *sync.WaitGroup) {
+	for num := s.Get(); num <= limit; num = s.Get() {
+
+		for i := uint64(1); i != calcRange; i++ {
+			num += 1
+			if isPrime(big.NewInt(int64(num))) {
+				fmt.Println(num)
+			}
+			//fmt.Println(i, num)
+
 		}
+
 	}
+	os.Exit(0)
+	//wg.Done()
 }
 
-func isPrime(i uint64) bool {
-	for j := uint64(3); j*j <= i; j += uint64(2) {
-		if i%j == 0 {
+func isPrime(p *big.Int) bool {
+	zero := big.NewInt(0)
+	one := big.NewInt(1)
+	two := big.NewInt(2)
+
+	if p.Cmp(two) == 0 {
+		return true
+	}
+
+	// p - 1 = 2^s * dに分解する
+	d := new(big.Int).Sub(p, one)
+	s := 0
+	for new(big.Int).And(d, one).Cmp(zero) == 0 {
+		d.Rsh(d, 1)
+		s++
+	}
+
+	n := new(big.Int).Sub(p, one)
+	k := 20
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := 0; i < k; i++ {
+		result := false
+		// [1, n-1]からaをランダムに選ぶ
+		a := new(big.Int).Rand(rnd, n)
+		a.Add(a, one)
+
+		// a^{2^r * d} mod p != -1(= p - 1 = n)の比較
+		tmp := new(big.Int).Exp(a, d, p)
+		for r := 0; r < s; r++ {
+			if tmp.Cmp(n) == 0 {
+				result = true
+				break
+			}
+			tmp.Exp(tmp, two, p)
+		}
+
+		// a^d != 1 mod p の比較
+		if !result && new(big.Int).Exp(a, d, p).Cmp(one) != 0 {
 			return false
 		}
 	}
+
 	return true
 }
